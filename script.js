2a2b9cf16ddfa748ccf6a0ecbf8919cdacdb7da5
Adrian Szeto <adrianduongszeto@gmail.com>
2022-07-13 21:50:20 -0700

    created more files to add scores and display questions

---
+++ script.js
@@ -0,0 +1,26 @@
+var username;
+var score = 0;
+
+var timer = 60;
+var realTimeEl = document.querySelector("#realTime");
+
+//DIV elements
+
+
+var questions = [
+    {
+        question: "Who is the current best korean bboy powermover?",
+        choices: ["Bboy Kill", "Bboy the End", "Bboy Shigekix", "Bboy Vero"],
+        answer: "Bboy the End"
+    },
+    {
+        question: "Who won the 2011 RedBull BC One World Finals?",
+        choices: ["Bboy Taisuke", "Bboy Hong10", "Bboy Roxrite", "Bboy Yan the Shrimp"],
+        answer: "Bboy Roxrite"
+    },
+    {
+        question: "Where is the RockForce crew originally founded?",
+        choices: ["Union City", "Oakland", "San Francisco", "South San Francisco"],
+        answer: "Union City"
+    },
+];
